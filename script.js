e585bd8b4eb94ab6ddad187496bb4eda65e6ef5d
Atif Haque <61943868+atifih@users.noreply.github.com>
2020-10-01 13:21:15 +1000

    made ajax call

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var today = moment().format("Do MMM YYYY");
+var today = moment().format("D/MM/YYYY");
 
 var day1 = moment().add(1, "days").calendar("DD MMM YYYY");
 var day2 = moment().add(2, "days").calendar("DD MM YYYY")
@@ -12,7 +12,8 @@ $("#day3Button").text(day3);
 $("#day4Button").text(day4);
 $("#day5Button").text(day5);
 
-
+var lat;
+var long;
 
 $("#run-search").on("click", function (e) {
 
@@ -28,12 +29,35 @@ $("#run-search").on("click", function (e) {
         url: queryURL,
         method: "GET",
     }).then(function (response) {
-        $("#weatherToday").append(JSON.stringify(response));
-        console.log(response);
+        //  $("#weatherToday").append(JSON.stringify(response));
+        // console.log(response);
         $("#todayW").append((response.name));
 
-        var icon_id = rdesponse.weather[0].icon;
+        var icon_id = response.weather[0].icon;
         var icon_url = "http://openweathermap.org/img/w/" + icon_id + ".png";
+        $("#todayW").append("(" + today + ")");
         $("#todayW").append(`<img src="${icon_url}">`);
+        $("#tempToday").append(response.main.temp);
+        $("#humidityToday").append(response.main.humidity)
+        $("#windToday").append(response.wind.speed);
+        lat = "" + response.coord.lat;
+        long = "" + response.coord.lon;
+
+
+        console.log(lat);
+        console.log(long);
+        //  $("#weatherToday").append("UV Index: " + response.main.uv);
+
+
     })
+    // Make a second ajax call for uv .
+    var queryURL2 = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIkey}&lat=${lat}&lon=${long}`
+    console.log(queryURL2);
+    $.ajax({
+        url: queryURL2,
+        method: "GET",
+    }).then(function (response2) {
+        console.log("Response 2 is :" + response2);
+    })
+
 })
